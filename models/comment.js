c33e5612bb8a7972a510bc0cfeddf238d6dfa67f
AnnaPalii <annapalii@yahoo.com>
2021-01-03 20:06:44 -0500

    added home,dashboard, intakeForm pages and few new components

--- models/comment.js
+++ models/comment.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const commentSchema = new Schema({
 	username: { type: String, required: true },
 	body: { type: String, required: true },
-	date: { type: Date, default: Date.now },
+	date: { type: Date, default: Date.now }
 });
 
 const Comment = mongoose.model("Comment", commentSchema);
