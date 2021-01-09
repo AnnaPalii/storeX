51f28101ba8fff4a46c06af937f1d80e0c1a9f40
Dannette Tejeda <dannettetejeda@gmail.com>
2021-01-08 23:36:56 -0500

    sidebar and account info components

---
+++ client/src/components/AccountInfo/AccountInfo.js
@@ -0,0 +1,86 @@
+/*
+
+
+      <div class="account-hub-content">
+        <div class="section-header">
+          <div class="section-header-info">
+            <p class="section-pretitle">Account</p>
+            <h2 class="section-title">Account Info</h2>
+          </div>
+        </div>
+        <div class="grid-column">
+          <div class="widget-box">
+            <p class="widget-box-title">Personal Info</p>
+            <div class="widget-box-content">
+           
+              <form class="form">
+       
+                <div class="form-row split">
+        
+                  <div class="form-item">
+ 
+                    <div class="form-input small active">
+                      <label for="account-full-name">Full Name</label>
+                      <input type="text" id="account-full-name" name="account_full_name">
+                    </div>
+         
+                  </div>
+            
+
+                  <div class="form-item">
+             
+                    <div class="form-input small active">
+                      <label for="account-email">Account Email</label>
+                      <input type="text" id="account-email" name="account_email">
+                    </div>
+                  </div>
+                </div>
+
+                <div class="form-row split">
+                  <div class="form-item">
+                    <div class="form-input small active">
+                      <label for="account-city">City</label>
+                      <input type="text" id="account-city" name="account_city">
+                    </div>
+                  </div>
+                  <div class="form-item">
+                    <div class="form-input small">
+                      <label for="account-phone">State</label>
+                      <select id="account-state" name="account_state">
+                        <option value="0">Select your State</option>
+                        <option value="1" selected>NY</option>
+                        <option value="2" selected>NJ</option>
+                      </select>
+
+                      <svg class="form-select-icon icon-small-arrow">
+                        <use xlink:href="#svg-small-arrow"></use>
+                      </svg>
+       
+                    </div>
+    
+                  </div>
+    
+
+                <div class="form-row split">
+      
+                  <div class="form-item">
+          
+                    <div class="form-select">
+                      <label for="account-country">Country</label>
+                      <input type="text" id="account-country" name="account_country" value="United States">
+                    </div>
+                  </div>
+        
+                  <div class="form-item">
+                    <div class="form-select">
+                      <label for="account-language">Phone Number</label>
+                      <input type="text" id="account-country" name="account_country" value="United States">
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>     
+      </div>
+    </div>
+  </div>
---
+++ client/src/components/SideBar/index.js
@@ -0,0 +1,82 @@
+
+        
+
+        <div class="grid grid-3-9 medium-space">
+        <div class="account-hub-sidebar">
+        <div class="sidebar-box no-padding">
+          <div class="sidebar-menu">
+            <div class="sidebar-menu-item">
+              <div class="sidebar-menu-header accordion-trigger-linked">
+                <svg class="sidebar-menu-header-icon icon-profile">
+                  <use xlink:href="#svg-profile"></use>
+                </svg>
+                <div class="sidebar-menu-header-control-icon">
+                  <svg class="sidebar-menu-header-control-icon-open icon-minus-small">
+                    <use xlink:href="#svg-minus-small"></use>
+                  </svg>
+                  <svg class="sidebar-menu-header-control-icon-closed icon-plus-small">
+                    <use xlink:href="#svg-plus-small"></use>
+                  </svg>
+                </div>
+              </div>
+            </div>
+
+            <div class="sidebar-menu-item">
+              <div class="sidebar-menu-header accordion-trigger-linked">
+                <svg class="sidebar-menu-header-icon icon-settings">
+                  <use xlink:href="#svg-settings"></use>
+                </svg>
+                <div class="sidebar-menu-header-control-icon">
+                  <svg class="sidebar-menu-header-control-icon-open icon-minus-small">
+                    <use xlink:href="#svg-minus-small"></use>
+                  </svg>
+                  <svg class="sidebar-menu-header-control-icon-closed icon-plus-small">
+                    <use xlink:href="#svg-plus-small"></use>
+                  </svg>
+                </div>
+
+                <p class="sidebar-menu-header-title">Account</p>
+              </div>
+              <div class="sidebar-menu-body accordion-content-linked accordion-open">
+                <a class="sidebar-menu-link active" href="hub-account-info.html">Account Info</a>
+              </div>
+            </div>
+
+            
+            <div class="sidebar-menu-item">
+          
+              <div class="sidebar-menu-header accordion-trigger-linked">
+  
+                <svg class="sidebar-menu-header-icon icon-group">
+                  <use xlink:href="#svg-group"></use>
+                </svg>
+    
+                <div class="sidebar-menu-header-control-icon">
+                
+                  <svg class="sidebar-menu-header-control-icon-open icon-minus-small">
+                    <use xlink:href="#svg-minus-small"></use>
+                  </svg>
+
+                  <svg class="sidebar-menu-header-control-icon-closed icon-plus-small">
+                    <use xlink:href="#svg-plus-small"></use>
+                  </svg>
+                
+                </div>
+                
+            </div>
+                <p class="sidebar-menu-header-title">Manage Spaces</p>
+              </div>
+              <div class="sidebar-menu-body accordion-content-linked">
+                <a class="sidebar-menu-link" href="hub-store-account.html">Bookings</a>
+                <a class="sidebar-menu-link" href="hub-store-statement.html">Listings</a>
+                <a class="sidebar-menu-link" href="hub-store-items.html">Messages</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="sidebar-box-footer">
+            <p class="button primary">Save Changes!</p>
+            <p class="button white small-space">Discard All</p>
+          </div>
+        </div>
+      </div>
