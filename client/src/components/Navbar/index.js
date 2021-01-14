0c42a835330ac7ee4b0a406d7968dc6f4f016e36
Paige Burlew <paigeburlew@gmail.com>
2021-01-13 21:10:39 -0600

    added about us page because it is in nav bar

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -43,7 +43,7 @@ function Navbar () {
             </li>
             <li className='nav-item'>
                 <Link
-                to='/services'
+                to='/aboutus'
                 className='nav-links'
                 onClick={closeMobileMenu}
                 >
